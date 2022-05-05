02c7d405fb0cccd386034c95408a224a1882883b
Adriano Silva <adriano.silva@maximatech.com.br>
2022-05-05 02:10:03 -0300

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  production: false,
   apiUrlv1: 'https://tcc-adiano.azurewebsites.net/api',
   imgUrl: 'https://tcc-adiano.azurewebsites.net/imgs',
 };
