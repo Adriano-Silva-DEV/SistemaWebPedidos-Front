cb21b70cee686a0a81484d160f74208dbd5ad1a7
ADRIANO SILVA <adrianosilvaxp@gmail.com>
2022-08-02 18:05:08 -0300

    feat: ajuste environment

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+
+export const environment = {
+  production: false,
+  apiUrlv1: 'https://localhost:44334/api',
+  imgUrl: 'https://localhost:44334/imgs'
+};
--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrlv1: 'https://tcc-adriano.azurewebsites.net/api',
-  imgUrl: 'https://tcc-adriano.azurewebsites.net/imgs',
-};
